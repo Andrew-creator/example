9e154df05ca8bc7cefe91d83dbe94b7cfefbb0ae
Andrew-creator <54023084+Andrew-creator@users.noreply.github.com>
2022-12-09 09:36:27 +0400

    Update example1.c

--- example1.c
+++ example1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-  for (int I=1; I<100; I++) {
-    if ((I%15)==0) printf("FizzBuzz\n");
-    else if ((I%3)==0) printf("Fizz\n");
-    else if ((I%5)==0) printf("Buzz\n");
+  for (int i=1; i<100; i++) {
+    if ((i%15)==0) printf("FizzBuzz\n");
+    else if ((i%3)==0) printf("Fizz\n");
+    else if ((i%5)==0) printf("Buzz\n");
     else printf("%d\n",i);
   }
 };
